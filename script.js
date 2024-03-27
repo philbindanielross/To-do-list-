b35145077d0a8e6db7305001b001ab5deb2e5556
philbindanielross <philbindanielr@gmail.com>
2024-03-26 19:14:44 -0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,32 @@
+function newItem() {
+  //javascript
+  //1. Adding a new item to the list of items:
+
+  let li = $("<li></li>");
+  let inputValue = $("#input").val();
+  //let text = document.createTextNode(inputValue);
+  li.append(inputValue);
+
+  if (inputValue === "") {
+    alert("You must write something!");
+  } else {
+    let list = $("#list");
+    list.append(li);
+  }
+
+  //2. Crossing out an item from the list of items:
+
+  li.on("dblclick", function () {
+    li.toggleClass("strike");
+  });
+
+  //3(i). Adding the delete button "X":
+  let crossOutButton = $("<button>X</button>");
+  li.append(crossOutButton);
+  crossOutButton.on("click", function () {
+    li.addClass("delete");
+  });
+
+  // 4. Reordering the items:
+  $("#list").sortable();
+}
